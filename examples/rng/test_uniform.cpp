f89457867a6d39c995988c8734ea295c44c3cea2
Kun Wang <wk910930@gmail.com>
2016-10-12 18:25:34 +0800

    remove iostream for lint issue

--- examples/rng/test_uniform.cpp
+++ examples/rng/test_uniform.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "caffe/util/math_functions.hpp"
 
 int main(int argc, char* argv[]) {
--- tools/convert_net_proto_binary_to_text.cpp
+++ tools/convert_net_proto_binary_to_text.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "caffe/util/io.hpp"
 
 int main(int argc, char* argv[]) {
