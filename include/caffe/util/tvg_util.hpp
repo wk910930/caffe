488edcff4fc13cf4b6274e4bcfd9ae6242004058
Kun Wang <wk910930@gmail.com>
2017-03-07 10:32:51 +0800

    formatting tvg_util.hpp

--- include/caffe/util/tvg_util.hpp
+++ include/caffe/util/tvg_util.hpp
@@ -9,13 +9,10 @@ namespace caffe {
 
 template <typename Dtype>
 void PrintBlob(const Blob<Dtype>* blob, bool print_diff = false, const char* info = 0) {
-
   const Dtype* data = print_diff ? blob->cpu_diff() : blob->cpu_data();
-
   if (info != 0) {
     printf("%s: \n", info);
   }
-
   for (int n = 0; n < blob->num(); n++) {
     for (int c = 0; c < blob->channels(); c++) {
       for (int h = 0; h < blob->height(); h++) {
@@ -27,22 +24,15 @@ void PrintBlob(const Blob<Dtype>* blob, bool print_diff = false, const char* inf
       }
       printf("\n");
     }
-   // printf("=================\n");
   }
-
   printf("-- End of Blob --\n\n");
 }
-
 template void PrintBlob(const Blob<float>* blob, bool print_diff = false, const char* info = 0);
-
 template void PrintBlob(const Blob<double>* blob, bool print_diff = false, const char* info = 0);
-
 
 template <typename Dtype>
 void FillWithMax(Blob<Dtype>* blob, float max_value = 1) {
-
   srand(2000);
-
   for (int i = 0; i < blob->count(); ++i) {
     blob->mutable_cpu_data()[i] = ((double) rand() / RAND_MAX) * max_value;
   }
@@ -50,12 +40,9 @@ void FillWithMax(Blob<Dtype>* blob, float max_value = 1) {
 template void FillWithMax(Blob<float>* const blob, float max_value = 1);
 template void FillWithMax(Blob<double>* const blob, float max_value = 1);
 
-
 template <typename Dtype>
 void FillAsRGB(Blob<Dtype>* blob) {
-
   srand(2000);
-
   for (int i = 0; i < blob->count(); ++i) {
     blob->mutable_cpu_data()[i] = rand() % 256;
   }
@@ -65,24 +52,18 @@ template void FillAsRGB(Blob<double>* const blob);
 
 template<typename Dtype>
 void FillAsProb(Blob<Dtype>* blob) {
-
-  srand(1000);//time(NULL));
-
+  srand(1000);  // time(NULL));
   for (int i = 0; i < blob->count(); ++i) {
     double num = (double) rand() / (double) RAND_MAX;
     blob->mutable_cpu_data()[i] = static_cast<Dtype>((num != 0) ? num : 0.0002);
   }
-
   for (int n = 0; n < blob->num(); ++n) {
     for (int h = 0; h < blob->height(); ++h) {
       for (int w = 0; w < blob->width(); ++w) {
-
         Dtype total = 0;
-
         for (int c = 0; c < blob->channels(); ++c) {
           total += blob->data_at(n, c, h, w);
         }
-
         for (int c = 0; c < blob->channels(); ++c) {
           blob->mutable_cpu_data()[blob->offset(n, c, h, w)] = blob->data_at(n, c, h, w) / total;
         }
@@ -93,11 +74,9 @@ void FillAsProb(Blob<Dtype>* blob) {
 template void FillAsProb(Blob<float>* const blob);
 template void FillAsProb(Blob<double>* const blob);
 
-
 template<typename Dtype>
 void FillAsLogProb(Blob<Dtype>* blob) {
   FillAsProb(blob);
-
   for (int i = 0; i < blob->count(); ++i) {
     blob->mutable_cpu_data()[i] = log(blob->cpu_data()[i]);
   }
@@ -105,4 +84,4 @@ void FillAsLogProb(Blob<Dtype>* blob) {
 template void FillAsLogProb(Blob<float>* const blob);
 template void FillAsLogProb(Blob<double>* const blob);
 
-}
+}  // namespace caffe
