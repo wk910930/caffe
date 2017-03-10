4cd105f438e653da9c4d08de9f733046c06f5a63
Kun Wang <wk910930@gmail.com>
2017-03-10 19:43:55 +0800

    formatting modified_permutohedral.cu

--- src/caffe/util/modified_permutohedral.cu
+++ src/caffe/util/modified_permutohedral.cu
@@ -7,29 +7,24 @@
 
 namespace caffe {
 
-static void swapHashTableValues(float* oldValues, float *newValues, float* table_values,size_t size) {
-  CUDA_CHECK(cudaMemcpy(oldValues,table_values,size,cudaMemcpyDeviceToDevice));
-  CUDA_CHECK(cudaMemcpy(table_values,newValues,size,cudaMemcpyDeviceToDevice));
-  CUDA_CHECK(cudaMemcpy(newValues,oldValues,size,cudaMemcpyDeviceToDevice));
+static void swapHashTableValues(float* oldValues, float *newValues, float* table_values, size_t size) {
+  CUDA_CHECK(cudaMemcpy(oldValues, table_values, size, cudaMemcpyDeviceToDevice));
+  CUDA_CHECK(cudaMemcpy(table_values, newValues, size, cudaMemcpyDeviceToDevice));
+  CUDA_CHECK(cudaMemcpy(newValues, oldValues, size, cudaMemcpyDeviceToDevice));
   // Works but give poorer results
-  //oldValues = table_values;
-  //table_values = newValues;
-  //newValues = oldValues;
+  // oldValues = table_values;
+  // table_values = newValues;
+  // newValues = oldValues;
 }
 
 template<int pd>
 __global__ static void createMatrix(const int w, const int h,
-    const float *positions,
-    int *table_entries,
-    int table_capacity,
-    signed short* table_keys,
-    const float *scaleFactor,
-    MatrixEntry *matrix)
-{
+    const float *positions, int *table_entries, int table_capacity,
+    signed short* table_keys, const float *scaleFactor, MatrixEntry *matrix) {
   // scanline order
-  //const int idx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
-  //const bool outOfBounds = (idx>=num_points) ;
-  //const int threadId = idx;
+  // const int idx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+  // const bool outOfBounds = (idx>=num_points) ;
+  // const int threadId = idx;
 
   // 8x8 blocks
   const int x = threadIdx.x + blockIdx.x * blockDim.x;
@@ -49,7 +44,6 @@ __global__ static void createMatrix(const int w, const int h,
   short *myKey = keys + threadId * pd;
 
   if (!outOfBounds) {
-
     myElevated[pd] = -pd*(myPosition[pd-1])*scaleFactor[pd-1];
     for (int i = pd-1; i > 0; i--) {
       myElevated[i] = (myElevated[i+1] -
@@ -57,10 +51,7 @@ __global__ static void createMatrix(const int w, const int h,
           (i+2)*(myPosition[i])*scaleFactor[i]);
     }
     myElevated[0] = myElevated[1] + 2*(myPosition[0])*scaleFactor[0];
-
-
     // find the closest zero-colored lattice point
-
     // greedily search for the closest zero-colored lattice point
     signed short sum = 0;
     for (int i = 0; i <= pd; i++) {
@@ -87,8 +78,7 @@ __global__ static void createMatrix(const int w, const int h,
         }
       }
     }
-
-    if (sum > 0) { // sum too large, need to bring down the ones with the smallest differential
+    if (sum > 0) {  // sum too large, need to bring down the ones with the smallest differential
       for (int i = 0; i <= pd; i++) {
         if (myRank[i] >= pd + 1 - sum) {
           myGreedy[i] -= pd+1;
@@ -97,7 +87,7 @@ __global__ static void createMatrix(const int w, const int h,
           myRank[i] += sum;
         }
       }
-    } else if (sum < 0) { // sum too small, need to bring up the ones with largest differential
+    } else if (sum < 0) {  // sum too small, need to bring up the ones with largest differential
       for (int i = 0; i <= pd; i++) {
         if (myRank[i] < -sum) {
           myGreedy[i] += pd+1;
@@ -107,12 +97,10 @@ __global__ static void createMatrix(const int w, const int h,
         }
       }
     }
-
     // turn delta into barycentric coords
     for (int i = 0; i <= pd+1; i++) {
       myBarycentric[i] = 0;
     }
-
     for (int i = 0; i <= pd; i++) {
       float delta = (myElevated[i] - myGreedy[i]) * (1.0f/(pd+1));
       myBarycentric[pd-myRank[i]] += delta;
@@ -130,7 +118,6 @@ __global__ static void createMatrix(const int w, const int h,
         if (myRank[i] > pd-color) myKey[i] -= (pd+1);
       }
     }
-
     if (!outOfBounds) {
       MatrixEntry r;
       r.index = hashTableInsert<pd>(myKey, table_keys, table_entries,
@@ -142,18 +129,14 @@ __global__ static void createMatrix(const int w, const int h,
 }
 
 template<int kd>
-__global__ static void cleanHashTable(const int n,
-    int *table_entries,
-    int table_capacity,
-    signed short* table_keys)
-{
+__global__ static void cleanHashTable(const int n, int *table_entries,
+    int table_capacity, signed short* table_keys) {
   const int idx = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x * blockDim.y + threadIdx.x;
-
-  if (idx >= n) return;
-
+  if (idx >= n) {
+    return;
+  }
   // find my hash table entry
   int *e = table_entries + idx;
-
   // Check if I created my own key in the previous phase
   if (*e >= 0) {
     // Rehash my key and reset the pointer in order to merge with
@@ -169,15 +152,13 @@ __global__ static void cleanHashTable(const int n,
 
 template<int pd>
 __global__ static void resetIndex(const int w, const int h,
-    MatrixEntry *matrix,
-    int *table_entries)
-{
+    MatrixEntry *matrix, int *table_entries) {
   const int x = threadIdx.x + blockIdx.x * blockDim.x;
   const int y = threadIdx.y + (blockIdx.y/(pd+1)) * blockDim.y;
   const int color = blockIdx.y % (pd+1);
   const int idx = y*w + x;
   const bool outOfBounds = (x >= w) || (y >= h);
-  if (!outOfBounds){
+  if (!outOfBounds) {
     MatrixEntry r = matrix[idx*(pd+1)+color];
     matrix[idx*(pd+1)+color].index = table_entries[r.index];
   }
@@ -185,10 +166,7 @@ __global__ static void resetIndex(const int w, const int h,
 
 template<int pd, typename Dtype>
 __global__ static void splatCache(const int w, const int h, const int vd,
-    const Dtype *values,
-    const MatrixEntry *matrix,
-    float *table_values)
-{
+    const Dtype *values, const MatrixEntry *matrix, float *table_values) {
   const int x = threadIdx.x + blockIdx.x * blockDim.x;
   const int y = threadIdx.y + (blockIdx.y/(pd+1)) * blockDim.y;
   const int threadId = threadIdx.y*blockDim.x + threadIdx.x;
@@ -202,30 +180,24 @@ __global__ static void splatCache(const int w, const int h, const int vd,
   float *myValue = sharedValues + threadId*(vd+1);
 
   if (!outOfBounds) {
-
     const Dtype *value = values + idx;
-
     MatrixEntry r = matrix[idx*(pd+1)+color];
-
     // convert the matrix entry from a pointer into the entries array to a pointer into the keys/values array
-    //matrix[idx*(pd+1)+color].index = r.index = table_entries[r.index];
+    // matrix[idx*(pd+1)+color].index = r.index = table_entries[r.index];
     // record the offset into the keys/values array in shared space
     myOffset = sharedOffsets[threadId] = r.index*(vd+1);
-
     for (int j = 0; j < vd; j++) {
       myValue[j] = (float)value[j*w*h]*r.weight;
     }
     myValue[vd] = r.weight;
-
   } else {
     sharedOffsets[threadId] = -1;
   }
-
   __syncthreads();
-
   // am I the first thread in this block to care about this key?
-
-  if (outOfBounds) return;
+  if (outOfBounds) {
+    return;
+  }
 
   for (int i = 0; i < BLOCK_SIZE; i++) {
     if (i < threadId) {
@@ -252,21 +224,16 @@ __global__ static void splatCache(const int w, const int h, const int vd,
 
 template<int pd>
 __global__ static void blur(int n, float *newValues,
-    const MatrixEntry *matrix,
-    const int *table_entries,
-    const signed short *table_keys,
-    const int table_capacity,
-    float *table_values,
-    int color,
-    const int vd)
-{
+    const MatrixEntry *matrix, const int *table_entries, const signed short *table_keys,
+    const int table_capacity, float *table_values, int color, const int vd) {
   const int idx = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x * blockDim.y + threadIdx.x;
-
-  if (idx >= n) return;
-
+  if (idx >= n) {
+    return;
+  }
   // Check if I'm valid
-  if (matrix[idx].index != idx) return;
-
+  if (matrix[idx].index != idx) {
+    return;
+  }
   // find my key and the keys of my neighbours
   short myKey[pd+1];
   short np[pd+1];
@@ -277,7 +244,6 @@ __global__ static void blur(int n, float *newValues,
     np[i] = myKey[i]+1;
     nm[i] = myKey[i]-1;
   }
-
 
   np[color] -= pd+1;
   nm[color] += pd+1;
@@ -311,20 +277,18 @@ __global__ static void blur(int n, float *newValues,
 
 template<int pd, typename Dtype>
 __global__ static void slice(const int w, const int h, const int vd,
-    Dtype *values,
-    const MatrixEntry *matrix,
-    float *table_values,
-    bool add) {
-  //const int idx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
-
+    Dtype *values, const MatrixEntry *matrix,
+    float *table_values, bool add) {
+  // const int idx = blockIdx.x * BLOCK_SIZE + threadIdx.x;
   const int x = threadIdx.x + blockIdx.x * blockDim.x;
   const int y = threadIdx.y + blockIdx.y * blockDim.y;
   const int threadId = threadIdx.y*blockDim.x + threadIdx.x;
   const int idx = y*w + x;
   const bool outOfBounds = (x >= w) || (y >= h);
 
-  if (outOfBounds) return;
-
+  if (outOfBounds) {
+    return;
+  }
   extern __shared__ float localValue[];
 
   float *myValue = localValue + threadId*vd;
@@ -343,10 +307,10 @@ __global__ static void slice(const int w, const int h, const int vd,
     myWeight += r.weight*val[vd];
   }
 
-  //myWeight = 1.0f/myWeight;
+  // myWeight = 1.0f/myWeight;
   float alpha = 1.0f / (1+powf(2, -pd));
-  for (int j = 0; j < vd; j++){
-    if(!add){
+  for (int j = 0; j < vd; j++) {
+    if (!add) {
       values[j*w*h + idx] = 0;
     }
     values[j*w*h + idx] += myValue[j]*alpha;
@@ -355,25 +319,21 @@ __global__ static void slice(const int w, const int h, const int vd,
 
 
 template<int pd>
-void gpu_init(const float* features,
-    HashTable* table,
-    MatrixEntry* matrix,
-    const int w, const int h)
-{
-  int num_points = w*h ;
+void gpu_init(const float* features, HashTable* table, MatrixEntry* matrix,
+    const int w, const int h) {
+  int num_points = w*h;
   // Scan line order
-  //unsigned int blocks = (num_points-1)/64 + 1;
-  //unsigned int blockSize = 64;
+  // unsigned int blocks = (num_points-1)/64 + 1;
+  // unsigned int blockSize = 64;
   dim3 blocks((w-1)/8+1, (h-1)/8+1, 1);
   dim3 blockSize(8, 8, 1);
 
-  float blurVariance = 0.5 ;
+  float blurVariance = 0.5;
   float * scaleFactor;
   float* scaleFactorHost = new float[pd];
 
   // Create Scale factor vector and give it to GPU
-  // num_dimensions is likely to be low so do that
-  // on the CPU
+  // num_dimensions is likely to be low so do that on the CPU
   for (int i = 0; i < pd; i++) {
     scaleFactorHost[i] = (pd+1)*sqrtf((1.0/6 + blurVariance)/((i+1)*(i+2)));
   }
@@ -397,7 +357,7 @@ void gpu_init(const float* features,
   CUDA_POST_KERNEL_CHECK;
 
   blocks.y *= pd+1;
-  resetIndex<pd><<<blocks, blockSize>>>(w, h, matrix, table->table_entries) ;
+  resetIndex<pd><<<blocks, blockSize>>>(w, h, matrix, table->table_entries);
   CUDA_POST_KERNEL_CHECK;
 
   // Clean intermediate variables
@@ -407,15 +367,12 @@ void gpu_init(const float* features,
 
 template<int pd, typename Dtype>
 void gpu_compute(Dtype* out, const Dtype* in, const HashTable &table,
-  const MatrixEntry* matrix,
-  int w, int h, int vd,
-  bool reverse, bool add){
-
+    const MatrixEntry* matrix, int w, int h, int vd, bool reverse, bool add) {
   // Create table_values
-  int num_points = w*h ;
-  float *table_values ;
-  CUDA_CHECK(cudaMalloc((void**)&table_values, sizeof(float)*(vd+1)*num_points*(pd+1))) ;
-  caffe_gpu_set<float>(num_points*(vd+1)*(pd+1), 0, table_values) ;
+  int num_points = w*h;
+  float *table_values;
+  CUDA_CHECK(cudaMalloc((void**)&table_values, sizeof(float)*(vd+1)*num_points*(pd+1)));
+  caffe_gpu_set<float>(num_points*(vd+1)*(pd+1), 0, table_values);
 
   dim3 blocks((w-1)/8+1, (h-1)/8+1, 1);
   dim3 blockSize(8, 8, 1);
@@ -436,8 +393,8 @@ void gpu_compute(Dtype* out, const Dtype* in, const HashTable &table,
   size_t size =  num_points*(pd+1)*(vd+1)*sizeof(float);
   CUDA_CHECK(cudaMalloc((void**)&(newValues), size));
   CUDA_CHECK(cudaMalloc((void**)&(oldValues), size));
-  caffe_gpu_set<float>(num_points*(vd+1)*(pd+1), 0, newValues) ;
-  for (int color = reverse?pd:0; color <= pd && color>=0; reverse?color--:color++) {
+  caffe_gpu_set<float>(num_points*(vd+1)*(pd+1), 0, newValues);
+  for (int color = reverse ? pd : 0; color <= pd && color >= 0; reverse ? color-- : color++) {
     blur<pd><<<cleanBlocks, cleanBlockSize>>>(num_points*(pd+1), newValues,
      matrix,
      table.table_entries,
@@ -450,31 +407,29 @@ void gpu_compute(Dtype* out, const Dtype* in, const HashTable &table,
     // swap pointers does not seem to work...
     swapHashTableValues(oldValues, newValues, table_values, size);
   }
-
   // slice
   blocks.y /= (pd+1);
   slice<pd, Dtype><<<blocks, blockSize, sizeof(float)*BLOCK_SIZE*vd>>>(w, h, vd, out, matrix, table_values, add);
   CUDA_POST_KERNEL_CHECK;
-
   // Free memory
-  CUDA_CHECK(cudaFree(table_values)) ;
-  CUDA_CHECK(cudaFree(newValues)) ;
-  CUDA_CHECK(cudaFree(oldValues)) ;
+  CUDA_CHECK(cudaFree(table_values));
+  CUDA_CHECK(cudaFree(newValues));
+  CUDA_CHECK(cudaFree(oldValues));
 }
 
 void ModifiedPermutohedral::init_gpu(const float* features, int num_dimensions, int w, int h) {
-  //Initialize Hash table
-  if(!is_init){
+  // Initialize Hash table
+  if (!is_init) {
     table.createHashTable(w*h*(num_dimensions+1), num_dimensions);
     CUDA_CHECK(cudaMalloc((void **)&matrix, sizeof(MatrixEntry)*(w*h*(num_dimensions+1))));
   } else {
     table.resetHashTable(w_*h_*(d_+1), d_);
   }
-  w_ = w ;
-  h_ = h ;
-  d_ = num_dimensions ;
-  N_ = w*h ;
-  switch(num_dimensions){
+  w_ = w;
+  h_ = h;
+  d_ = num_dimensions;
+  N_ = w*h;
+  switch (num_dimensions) {
     case 2:
       gpu_init<2>(features, &table, matrix, w_, h_);
       break;
@@ -488,9 +443,9 @@ void ModifiedPermutohedral::init_gpu(const float* features, int num_dimensions, 
 
 void ModifiedPermutohedral::compute_gpu(float* out, const float* in, int value_size, bool reverse, bool add) const {
   // Losing time by dynamically allocating memory but more general function
-  if(!is_init)
+  if (!is_init)
     LOG(FATAL) << "Initialize lattice before doing any computing";
-  switch(d_){
+  switch (d_) {
     case 2:
       gpu_compute<2, float>(out, in, table, matrix, w_, h_, value_size, reverse, add);
       break;
@@ -504,9 +459,9 @@ void ModifiedPermutohedral::compute_gpu(float* out, const float* in, int value_s
 
 void ModifiedPermutohedral::compute_gpu(double* out, const double* in, int value_size, bool reverse, bool add) const {
   // Losing time by dynamically allocating memory but more general function
-  if(!is_init)
+  if (!is_init)
     LOG(FATAL) << "Initialize lattice before doing any computing";
-  switch(d_){
+  switch (d_) {
     case 2:
       gpu_compute<2, double>(out, in, table, matrix, w_, h_, value_size, reverse, add);
       break;
@@ -518,4 +473,4 @@ void ModifiedPermutohedral::compute_gpu(double* out, const double* in, int value
   }
 }
 
-}//namespace caffe
+}  // namespace caffe
