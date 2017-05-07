5af53ca993a6df41e0d288134ac71c6b2c3efeb6
Kun Wang <wk910930@gmail.com>
2017-05-07 11:06:31 +0800

    Update calculate_prior_box_param.py

--- examples/ssd/utils/calculate_prior_box_param.py
+++ examples/ssd/utils/calculate_prior_box_param.py
@@ -5,14 +5,13 @@ import math
 if __name__ == '__main__':
     # minimum dimension of input image
     min_dim = 300
-    # scale_1 ==> 38 x 38
-    # scale_2 ==> 19 x 19
+    # scale_1 ==> 40 x 40
+    # scale_2 ==> 20 x 20
     # scale_3 ==> 10 x 10
     # scale_4 ==> 5 x 5
     # scale_5 ==> 3 x 3
-    # scale_6 ==> 1 x 1
-    mbox_source_layers = ['scale_1', 'scale_2', 'scale_3', 'scale_4', 'scale_5', 'scale_6']
-    mbox_source_layers_size = [38, 19, 10, 5, 3, 1]
+    mbox_source_layers = ['scale_1', 'scale_2', 'scale_3', 'scale_4', 'scale_5']
+    mbox_source_layers_size = [40, 20, 10, 5, 3]
     # in percent %
     min_ratio = 20
     max_ratio = 90
@@ -29,16 +28,14 @@ if __name__ == '__main__':
     max_sizes = [min_dim * 20 / 100.] + max_sizes
 
     # calculate steps
-    steps = [8, 16, 32, 64, 100, 300]
-    my_steps = [int(math.ceil(float(min_dim) / x)) for x in mbox_source_layers_size]
+    steps = [int(math.ceil(float(min_dim) / x)) for x in mbox_source_layers_size]
 
     print '====== Prior Box Param ======'
     print 'min_raio: {}'.format(min_ratio)
     print 'max_ratio: {}'.format(max_ratio)
     print '-' * 48
     for i in xrange(len(mbox_source_layers)):
-        print '[{}] min_size={:.1f} max_size={:.1f}'.format(
-            mbox_source_layers[i], min_sizes[i], max_sizes[i])
+        print '[{}] [{:2d} x {:2d}] min_size={:.1f}, max_size={:.1f}, step={}'.format(
+            mbox_source_layers[i], mbox_source_layers_size[i], mbox_source_layers_size[i],
+            min_sizes[i], max_sizes[i], steps[i])
     print '-' * 48
-    print 'steps={} (manually)'.format(steps)
-    print 'steps={} (approximate)'.format(my_steps)
