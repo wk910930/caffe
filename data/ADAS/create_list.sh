09a2c9950a5955eeaeee2ecee866e68dfdb06bc6
wang kun <wk910930@gmail.com>
2017-07-24 14:07:01 +0800

    Add ADAS

---
+++ data/ADAS/create_list.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+root_dir=$HOME/data/ADASdevkit/
+sub_dir=ImageSets/Main
+bash_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+for dataset in train_half test
+do
+  dst_file=$bash_dir/$dataset.txt
+  if [ -f $dst_file ]
+  then
+    rm -f $dst_file
+  fi
+  for name in ADAS01
+  do
+    echo "Create list for $name $dataset..."
+    dataset_file=$root_dir/$name/$sub_dir/$dataset.txt
+
+    img_file=$bash_dir/$dataset"_img.txt"
+    cp $dataset_file $img_file
+    sed -i "s/^/$name\/JPEGImages\//g" $img_file
+    sed -i "s/$/.jpg/g" $img_file
+
+    label_file=$bash_dir/$dataset"_label.txt"
+    cp $dataset_file $label_file
+    sed -i "s/^/$name\/Annotations\//g" $label_file
+    sed -i "s/$/.xml/g" $label_file
+
+    paste -d' ' $img_file $label_file >> $dst_file
+
+    rm -f $label_file
+    rm -f $img_file
+  done
+
+  # Generate image name and size infomation.
+  if [ $dataset == "test" ]
+  then
+    $bash_dir/../../build/tools/get_image_size $root_dir $dst_file $bash_dir/$dataset"_name_size.txt"
+  fi
+
+  # Shuffle train file.
+  if [ $dataset == "train_half" ]
+  then
+    rand_file=$dst_file.random
+    cat $dst_file | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);' > $rand_file
+    mv $rand_file $dst_file
+  fi
+done
