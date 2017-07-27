1d8d8bb36e0141698175800a8a22274083cbf9d0
Kun Wang <wk910930@gmail.com>
2017-07-27 19:47:33 +0800

    Add tool to convert between txt and binary proto files

---
+++ tools/convert_net_proto_binary_to_text.cpp
@@ -0,0 +1,13 @@
+#include "caffe/util/io.hpp"
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cout << "Usage: ./convert_net_proto_text_to_binary.bin"
+      << " net_proto_binary_file_in net_proto_text_file_out" << std::endl;
+    return -1;
+  }
+  caffe::NetParameter net_param;
+  caffe::ReadProtoFromBinaryFileOrDie(argv[1], &net_param);
+  caffe::WriteProtoToTextFile(net_param, argv[2]);
+  return 0;
+}
