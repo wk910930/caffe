1067871307c1d168e619734c991cbb35162b783b
Kun Wang <wk910930@gmail.com>
2018-01-27 00:59:00 +0800

    Fix error

--- data/svl/create_list.sh
+++ data/svl/create_list.sh
@@ -16,12 +16,12 @@ do
 
   img_file=$bash_dir/$dataset"_img.txt"
   cp $dataset_file $img_file
-  sed -i "s/^\/JPEGImages\//g" $img_file
+  sed -i "s/^/JPEGImages\//g" $img_file
   sed -i "s/$/.jpg/g" $img_file
 
   label_file=$bash_dir/$dataset"_label.txt"
   cp $dataset_file $label_file
-  sed -i "s/^/\/Annotations\//g" $label_file
+  sed -i "s/^/Annotations\//g" $label_file
   sed -i "s/$/.xml/g" $label_file
 
   paste -d' ' $img_file $label_file >> $dst_file
