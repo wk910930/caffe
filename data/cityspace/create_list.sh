affbf125858aed74780a1d7427a98b77f031cb54
Kun Wang <wk910930@gmail.com>
2018-04-12 23:52:54 +0800

    Little update

--- data/cityspace/create_list.sh
+++ data/cityspace/create_list.sh
@@ -10,17 +10,18 @@ do
   then
     rm -f $dst_file
   fi
+
   echo "Create list for $dataset..."
   dataset_file=$root_dir/$sub_dir/$dataset.txt
 
   img_file=$bash_dir/$dataset"_img.txt"
   cp $dataset_file $img_file
-  sed -i "s/^\/JPEGImages\//g" $img_file
+  sed -i "s/^/JPEGImages\//g" $img_file
   sed -i "s/$/.jpg/g" $img_file
 
   label_file=$bash_dir/$dataset"_label.txt"
   cp $dataset_file $label_file
-  sed -i "s/^\/Annotations\//g" $label_file
+  sed -i "s/^/Annotations\//g" $label_file
   sed -i "s/$/.xml/g" $label_file
 
   paste -d' ' $img_file $label_file >> $dst_file
